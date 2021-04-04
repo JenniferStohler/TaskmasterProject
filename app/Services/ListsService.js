387258970e8a18b3a6e1e1fb77df9ebe5c1f1d74
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-04-04 14:41:17 -0600

    taskmaster2.0

---
+++ app/Services/ListsService.js
@@ -0,0 +1,18 @@
+import { ProxyState } from "../AppState.js";
+import List from "../Models/List.js";
+import { saveState } from "../Utils/LocalStorage.js";
+
+class ListsService {
+  deleteList(id) {
+    ProxyState.lists = ProxyState.lists.filter(li => li.id != id)
+    saveState()
+  }
+  addList(newList) {
+    ProxyState.lists = [...ProxyState.lists, new List(newList.name)]
+    saveState()
+  }
+}
+
+
+export const listsService = new ListsService();
+
---
+++ app/Services/TasksService.js
@@ -0,0 +1,18 @@
+import { ProxyState } from "../AppState.js";
+import Task from "../Models/Task.js";
+import { saveState } from "../Utils/LocalStorage.js";
+
+class TasksService {
+  deleteTask(id) {
+    ProxyState.tasks = ProxyState.tasks.filter(li => li.id != id)
+    saveState()
+
+  }
+  addTask(newTask) {
+    ProxyState.tasks.push(new Task(newTask.name, newTask.listId))
+    saveState()
+    ProxyState.tasks = ProxyState.tasks
+  }
+}
+
+export const tasksService = new TasksService();
