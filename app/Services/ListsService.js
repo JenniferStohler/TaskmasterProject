da3060f1345b9bd34b65b17ca0b8a4143d6cfb25
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-04-04 22:25:07 -0600

    finished

--- app/Services/ListsService.js
+++ app/Services/ListsService.js
@@ -4,8 +4,28 @@ import { saveState } from "../Utils/LocalStorage.js";
 
 class ListsService {
   deleteList(id) {
-    ProxyState.lists = ProxyState.lists.filter(li => li.id != id)
-    saveState()
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success',
+        ProxyState.lists = ProxyState.lists.filter(li => li.id != id),
+        saveState()
+        )
+      
+        }
+    })
+  
+  
   }
   addList(newList) {
     ProxyState.lists = [...ProxyState.lists, new List(newList.name)]
